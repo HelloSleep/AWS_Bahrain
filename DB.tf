3ba8e4dcf94bb12617ce31eb1af443f7631198c9
sallyshin <tlstpwjdssj@gmail.com>
2022-06-28 17:37:48 +0900

    change bahrain db instance

--- DB.tf
+++ DB.tf
@@ -9,18 +9,18 @@ resource "aws_db_subnet_group" "global-subnet-group" {
   }
 }
 
-resource "aws_db_instance" "global-master-db" {
+resource "aws_db_instance" "bahrain-master-db" {
   allocated_storage     = 50
   max_allocated_storage = 80
   engine                = "mysql"
-  engine_version        = "5.7.22"
-  instance_class        = "db.t2.micro"
-  # name                  = "grooming_bahrainmasterdb"
-  username             = "admin"
-  password             = "password"
-  db_subnet_group_name = aws_db_subnet_group.global-subnet-group.id
-  multi_az             = true
-  skip_final_snapshot  = true
+  engine_version        = "5.7.25"
+  instance_class        = "db.t3.micro"
+  name                  = "grooming-bahrain-db"
+  username              = "admin"
+  password              = "password"
+  db_subnet_group_name  = aws_db_subnet_group.global-subnet-group.id
+  multi_az              = true
+  skip_final_snapshot   = true
   vpc_security_group_ids = [
     aws_security_group.global-private-sg-db.id
   ]
